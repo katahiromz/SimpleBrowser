68a3177e57dadb5e73b3f422bdc57aaa688ef2b1
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-04-16 20:13:05 +0900

    fix LoadDataFile 2

--- SimpleBrowser.cpp
+++ SimpleBrowser.cpp
@@ -794,9 +794,11 @@ BOOL LoadDataFile(HWND hwnd, const WCHAR *path, std::wstring& data)
             mstr_split(fields, lines[i], L"\t");
             if (fields.size() >= 3 && fields[2].c_str()[0] == L'#')
             {
-                INT n = _wtoi(&fields[2][1]);
-                if (n == ID_PRINT_PREVIEW)   // print preview
+                INT id = _wtoi(&fields[2][1]);
+                if (id == ID_PRINT_PREVIEW ||
+                    id == IDM_PRINTPREVIEW)
                 {
+                    // print preview
                     lines.erase(lines.begin() + i);
                     break;
                 }
